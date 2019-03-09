79523071e5091823d4dac8fa0b95e54fc69cf498
tv.unimol <doslovno@gmail.com>
2019-03-09 21:31:59 +0300

    add ViewGroup.inflate() extension

---
+++ app/src/main/java/dev/manifest/redddit/commons/Extensions.kt
@@ -0,0 +1,10 @@
+@file:JvmName("ExtensionsUtils")
+package dev.manifest.redddit.commons
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
