9bb2003cc93e4a7e1b5b130f803f951fa77d1822
tv.unimol <doslovno@gmail.com>
2019-03-10 11:46:33 +0300

    add synthetic properties generated

--- app/src/main/java/dev/manifest/redddit/news/NewsFragment.kt
+++ app/src/main/java/dev/manifest/redddit/news/NewsFragment.kt
@@ -7,25 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import dev.manifest.redddit.R
 import dev.manifest.redddit.commons.inflate
+import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment : Fragment() {
-
-    private var newsList: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = container?.inflate(R.layout.fragment_news)
-        newsList = view?.findViewById<RecyclerView?>(R.id.news_list)
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
-
-        return view
+        return container?.inflate(R.layout.fragment_news)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
+    }
 }
