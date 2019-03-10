ece1af867948e164a001bc8ff53068abbc924ed9
tv.unimol <doslovno@gmail.com>
2019-03-10 14:48:10 +0300

    add LoadingDelegateAdapter

---
+++ app/src/main/java/dev/manifest/redddit/commons/adapter/LoadingDelegateAdapter.kt
@@ -0,0 +1,20 @@
+package dev.manifest.redddit.commons.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.manifest.redddit.R
+import dev.manifest.redddit.commons.inflate
+
+class LoadingDelegateAdapter : ViewTypeDelegateAdapter {
+
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = TurnsViewHolder(parent)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: ViewType) {
+        TODO("not implemented")
+    }
+
+
+    class TurnsViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+        parent.inflate(R.layout.news_item_loading)
+    )
+}
