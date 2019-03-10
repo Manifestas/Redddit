f03de9063813b9fdadefb262e3230d0cf6861f40
tv.unimol <doslovno@gmail.com>
2019-03-10 21:46:03 +0300

    add NewsAdapter

---
+++ app/src/main/java/dev/manifest/redddit/commons/adapter/NewsAdapter.kt
@@ -0,0 +1,35 @@
+package dev.manifest.redddit.commons.adapter
+
+import android.util.SparseArray
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var items: ArrayList<ViewType>
+    private var delegateAdapters = SparseArray<ViewTypeDelegateAdapter>()
+    private val loadingItem = object : ViewType {
+        override fun getViewType() = AdapterConstants.LOADING
+    }
+
+    init {
+        delegateAdapters.put(AdapterConstants.LOADING, LoadingDelegateAdapter())
+        items = ArrayList()
+        items.add(loadingItem)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return delegateAdapters.get(viewType).onCreateViewHolder(parent)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        return delegateAdapters.get(getItemViewType(position)).onBindViewHolder(holder, this.items[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return this.items[position].getViewType()
+    }
+}
