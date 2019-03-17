2b274113ca1e43be093ce56779d1947deb0d06a4
tv.unimol <doslovno@gmail.com>
2019-03-17 16:01:45 +0300

    add addNews(), getNews(), clearAndAddNews()

--- app/src/main/java/dev/manifest/redddit/commons/adapter/NewsAdapter.kt
+++ app/src/main/java/dev/manifest/redddit/commons/adapter/NewsAdapter.kt
@@ -3,6 +3,7 @@ package dev.manifest.redddit.commons.adapter
 import android.util.SparseArray
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import dev.manifest.redddit.commons.RedditNewsItem
 
 class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -33,4 +34,33 @@ class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemViewType(position: Int): Int {
         return this.items[position].getViewType()
     }
+
+    fun addNews(news: List<RedditNewsItem>) {
+        // first remove loading and notify
+        val initPosition = items.size - 1
+        items.removeAt(initPosition)
+        notifyItemRemoved(initPosition)
+
+        // insert news and the loading at the end of the list
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeChanged(initPosition, items.size + 1 /* plus loading item */)
+    }
+
+    fun clearAndAddNews(news: List<RedditNewsItem>) {
+        items.clear()
+        notifyItemRangeRemoved(0, getLastPosition())
+
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeInserted(0, items.size)
+    }
+
+    fun getNews(): List<RedditNewsItem> {
+        return items
+            .filter { it.getViewType() == AdapterConstants.NEWS }
+            .map { it as RedditNewsItem }
+    }
+
+    private fun getLastPosition() = if (items.lastIndex == -1) 0 else items.lastIndex
 }
