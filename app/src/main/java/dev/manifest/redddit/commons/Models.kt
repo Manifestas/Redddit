3df01592e63e6ceaa168cd24a65007f46d606096
tv.unimol <doslovno@gmail.com>
2019-03-17 18:37:06 +0300

    add RedditNews model

--- app/src/main/java/dev/manifest/redddit/commons/Models.kt
+++ app/src/main/java/dev/manifest/redddit/commons/Models.kt
@@ -3,6 +3,11 @@ package dev.manifest.redddit.commons
 import dev.manifest.redddit.commons.adapter.AdapterConstants
 import dev.manifest.redddit.commons.adapter.ViewType
 
+data class RedditNews(
+    val after: String,
+    val before: String,
+    val news: List<RedditNewsItem>)
+
 data class RedditNewsItem(
     val author: String,
     val title: String,
