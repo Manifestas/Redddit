f13a0b4281f6b0271cb96abc6e38ae399764d0e9
tv.unimol <doslovno@gmail.com>
2019-03-17 16:14:19 +0300

    rename ViewHolders

--- app/src/main/java/dev/manifest/redddit/commons/adapter/LoadingDelegateAdapter.kt
+++ app/src/main/java/dev/manifest/redddit/commons/adapter/LoadingDelegateAdapter.kt
@@ -7,14 +7,14 @@ import dev.manifest.redddit.commons.extensions.inflate
 
 class LoadingDelegateAdapter : ViewTypeDelegateAdapter {
 
-    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = TurnsViewHolder(parent)
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder = LoadingViewHolder(parent)
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: ViewType) {
         TODO("not implemented")
     }
 
 
-    class TurnsViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+    class LoadingViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
         parent.inflate(R.layout.news_item_loading)
     )
 }
