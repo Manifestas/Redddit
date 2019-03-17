d61e668041e64f2aeecb45fc25a6b3ed3b55d4a0
tv.unimol <doslovno@gmail.com>
2019-03-17 17:38:35 +0300

    NewsManger getting realData

--- app/src/main/java/dev/manifest/redddit/news/NewsManager.kt
+++ app/src/main/java/dev/manifest/redddit/news/NewsManager.kt
@@ -1,27 +1,27 @@
 package dev.manifest.redddit.news
 
+import dev.manifest.redddit.api.RestAPI
 import dev.manifest.redddit.commons.RedditNewsItem
 import rx.Observable
 
-class NewsManager() {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create { subscriber ->
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
                     RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
+                        item.author, item.title, item.num_comments,
+                        item.created, item.thumbnail, item.url
                     )
-                )
+                }
+                subscriber.onNext(news)
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
-            subscriber.onCompleted()
         }
     }
 }
