3324dc2589eb1172e1aa27cfcb2c0b3f38614e2d
tv.unimol <doslovno@gmail.com>
2019-03-17 19:51:14 +0300

    getNews return RedditNews

--- app/src/main/java/dev/manifest/redddit/news/NewsManager.kt
+++ app/src/main/java/dev/manifest/redddit/news/NewsManager.kt
@@ -1,24 +1,39 @@
 package dev.manifest.redddit.news
 
 import dev.manifest.redddit.api.RestAPI
+import dev.manifest.redddit.commons.RedditNews
 import dev.manifest.redddit.commons.RedditNewsItem
 import rx.Observable
 
 class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
+    /**
+     * Returns Reddit News paginated by the given limit.
+     *
+     * @param after indicates the next page to navigate.
+     * @param limit the number of news to request.
+     */
+    fun getNews(after: String, limit: String = "10"): Observable<RedditNews> {
         return Observable.create { subscriber ->
-            val callResponse = api.getNews("", limit)
+            val callResponse = api.getNews(after, limit)
             val response = callResponse.execute()
             if (response.isSuccessful) {
-                val news = response.body().data.children.map {
+                val dataResponse = response.body().data
+                val news = dataResponse.children.map {
                     val item = it.data
                     RedditNewsItem(
                         item.author, item.title, item.num_comments,
                         item.created, item.thumbnail, item.url
                     )
                 }
-                subscriber.onNext(news)
+                val redditNews = RedditNews(
+                    dataResponse.after ?: "",
+                    dataResponse.before ?: "",
+                    news
+                )
+
+                subscriber.onNext(redditNews)
+
             } else {
                 subscriber.onError(Throwable(response.message()))
             }
