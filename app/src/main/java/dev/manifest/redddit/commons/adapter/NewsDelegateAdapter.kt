e134fc3025605d2c01d623484add1fbda5220d4b
tv.unimol <doslovno@gmail.com>
2019-03-17 15:43:18 +0300

    add NewsDelegateAdapter

---
+++ app/src/main/java/dev/manifest/redddit/commons/adapter/NewsDelegateAdapter.kt
@@ -0,0 +1,36 @@
+package dev.manifest.redddit.commons.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.manifest.redddit.R
+import dev.manifest.redddit.commons.RedditNewsItem
+import dev.manifest.redddit.commons.extensions.getFriendlyTime
+import dev.manifest.redddit.commons.extensions.inflate
+import dev.manifest.redddit.commons.extensions.loadImg
+import kotlinx.android.synthetic.main.news_item.view.*
+
+class NewsDelegateAdapter : ViewTypeDelegateAdapter {
+
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        return TurnsViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: ViewType) {
+        holder as TurnsViewHolder
+        holder.bind(item as RedditNewsItem)
+    }
+
+    class TurnsViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        parent.inflate(R.layout.news_item)
+    ) {
+
+        fun bind(item: RedditNewsItem) = with(itemView) {
+            //Picasso.with(itemView.context).load(item.thumbnail).into(img_thumbnail)
+            img_thumbnail.loadImg(item.thumbnail)
+            description.text = item.title
+            author.text = item.author
+            comments.text = "${item.numComments} comments"
+            time.text = item.created.getFriendlyTime()
+        }
+    }
+}
